b3521f8d074a612eb6f0f995707dc54ed0ada813
PEYRAT Damien <da.peyrat@gmail.com>
2018-04-20 11:28:18 +0200

    create UserBean and PostBean

--- chatClient/src/modele/PostBean.java
+++ chatClient/src/modele/PostBean.java
@@ -1,5 +1,25 @@
 package modele;
 
+import java.sql.Date;
+
 public class PostBean {
+	private String contenu;
+	private Date heure;
+
+	public String getContenu() {
+		return contenu;
+	}
+
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+
+	public Date getHeure() {
+		return heure;
+	}
+
+	public void setHeure(Date heure) {
+		this.heure = heure;
+	}
 
 }
--- chatClient/src/modele/UserBean.java
+++ chatClient/src/modele/UserBean.java
@@ -2,4 +2,23 @@ package modele;
 
 public class UserBean {
 
+	private String pseudo;
+	private Boolean connected;
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public Boolean getConnected() {
+		return connected;
+	}
+
+	public void setConnected(Boolean connected) {
+		this.connected = connected;
+	}
+
 }
